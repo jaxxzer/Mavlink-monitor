488cc17ff82a9ad6f7a422845554d68b227b3231
Jacob Walser <jwalser90@gmail.com>
2016-08-31 17:37:02 -0400

    Add pin definitions for V2 PCB

--- MapleMini.h
+++ MapleMini.h
@@ -1,11 +1,25 @@
 #ifndef MAPLE_MINI_H
 #define MAPLE_MINI_H
 
-#define ADC_VOLTAGE PA1
-#define ADC_CURRENT PA0
+// Onboard LEDS
 #define PIN_LED_MAPLE 33
-#define PIN_LED_1 -1
-#define PIN_LED_2 -1
-#define PIN_LED_3 -1
+#define PIN_LED_1 15
+#define PIN_LED_2 16
+#define PIN_LED_3 17
+
+// Power module battery monitoring
+#define PIN_ADC_VOLTAGE 11
+#define PIN_ADC_CURRENT 10
+
+// Onboard dip switch
+#define PIN_DIP_0 18 //LSB
+#define PIN_DIP_1 19
+#define PIN_DIP_2 21
+#define PIN_DIP_3 22 //MSB
+
+// Additional sensor inputs
+#define PIN_ADC_0 3
+#define PIN_ADC_1 4
+#define PIN_ADC_2 5
 
 #endif
