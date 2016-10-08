111af28dc247d95f6cc5a81d3d8085b4d6ec4e01
Jacob Walser <jwalser90@gmail.com>
2016-10-08 18:45:06 -0400

    WaterDetector: add debug option

--- WaterDetector.cpp
+++ WaterDetector.cpp
@@ -1,6 +1,8 @@
 #include "WaterDetector.h"
 #include "MapleMini.h"
 
+#define WATERDETECTOR_DEBUG 0
+
 WaterDetector::WaterDetector() :
 detected(false),
 last_detect_ms(0),
@@ -11,7 +13,9 @@ _update_interval_ms(50) // 20Hz
 	for(int i = 0; i < NUM_WATERDETECTORS; i++) {
 		detectors[i].pin = -1;
 	}
-	detectors[0].pin = PIN_WATERDETECTOR;
+
+	detectors[0].pin = PIN_WATERDETECTOR; // configure pin for waterdetector
+
 	for(int i = 0; i < NUM_WATERDETECTORS; i++) {
 		if(detectors[i].pin != -1) {
 			pinMode(detectors[i].pin, INPUT);
@@ -48,8 +52,18 @@ void WaterDetector::update() {
 			detectors[i].detected = !digitalRead(detectors[i].pin);
 			if(detectors[i].detected)
 				last_detect_ms = tnow;
+#if WATERDETECTOR_DEBUG
+		Serial.print("Waterdetector #");
+		Serial.print(i);
+		Serial.print(" state = ");
+		Serial.println(detectors[i].detected);
+#endif
 		}
 	}
 
 	detected = (tnow < last_detect_ms + WATERDETECTOR_COOLDOWN_MS) && (last_detect_ms != 0);
+#if WATERDETECTOR_DEBUG
+		Serial.print("Detected = ");
+		Serial.println(detected);
+#endif
 }
